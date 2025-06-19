ef0ec360bb5556e7c5a72e3f9589a7547f54d935
ShadowPrice1328 <kisukeurahara1328@gmail.com>
2025-06-19 19:46:32 +0200

    Header - Done

---
+++ inc/header.php
@@ -0,0 +1,18 @@
+<title>Anna Sonchak | Gallery</title>
+<meta charset="utf-8">
+
+<div class="header">
+    <p>Gallery of my life</p>
+    <svg style="margin-right: 3rem" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 72 72"><path fill="#0066CC" d="M5 17h62v38H5z"/><path fill="#FFD700" d="M5 36h62v19H5z"/><path fill="none" stroke-linejoin="round" d="M5 17h62v38H5z"/></svg>
+</div>
+
+<img class="header-photo" src="../tpl/images/header.JPG" alt="zadar_sunset">
+
+<div class="subheader">
+    <ul>
+        <li><a href="/"> Home </a> </li>
+        <li><a href="/"> About </a></li>
+        <li><a href="/"> GitHub </a></li>
+    </ul>
+</div>
+<link rel="stylesheet" href="../tpl/css/styles.css">
--- index.php
+++ index.php
@@ -4,13 +4,17 @@ $jpegs = glob(__DIR__ . "/tpl/images/*.jpeg");
 $pngs  = glob(__DIR__ . "/tpl/images/*.png");
 
 $images = array_merge($jpgs, $jpegs, $pngs);
+
+include __DIR__ . "/inc/header.php";
 ?>
 
 <body>
 <?php
-foreach ($images as $image) {
-    $filename = basename($image);
-    echo '<img src="/tpl/images/'.$filename.'" /><br />';
-}
-?>
+//foreach ($images as $image) {
+//    $filename = basename($image);
+//    echo '<img src="/tpl/images/'.$filename.'" /><br />';
+//}
+//?>
+
+
 </body>
